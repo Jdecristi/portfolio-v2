791ab1bd8282f8d1eb497e11dc338105dd87ef04
Josh DeCristi <jdecristi777a@gmail.com>
2023-04-10 23:19:27 -0400

    Add Open Source section

---
+++ src/layouts/OpenSource.svelte
@@ -0,0 +1,48 @@
+<section class="open-source">
+  <img class="blob" src="assets/blobs/blob-2.svg" alt="blob" />
+  <h1>Open Source</h1>
+  <p>
+    The concept of open source has played a significant role in my professional journey. In my view, software should
+    ideally be open source, but managing large-scale projects often presents challenges. I am currently exploring
+    opportunities to participate in new projects, and I invite you to peruse my GitHub profile for further information.
+  </p>
+  <a href="https://github.com/jdecristi" target="_blank">
+    <button class="large">Github</button>
+  </a>
+</section>
+
+<style lang="scss" global>
+  .open-source {
+    text-align: center;
+    padding: 10rem 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 5rem;
+    position: relative;
+
+    p {
+      font-size: $F200;
+      width: 60vw;
+      max-width: 800px;
+
+      @media (max-width: 900px) {
+        width: 80vw;
+      }
+    }
+
+    .blob {
+      position: absolute;
+      top: 50%;
+      left: 0;
+      width: 90vw;
+      transform: translateY(-50%);
+      z-index: -1;
+
+      @media (max-width: 900px) {
+        display: none;
+      }
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,13 @@
   import Experience from "@src/layouts/Experience.svelte";
   import Projects from "@src/layouts/projects.svelte";
   import Skills from "@src/layouts/Skills.svelte";
+  import OpenSource from "@src/layouts/OpenSource.svelte";
 </script>
 
 <AboutMe />
 <Experience />
 <Projects />
 <Skills />
+<OpenSource />
 
 <style lang="scss" global></style>
