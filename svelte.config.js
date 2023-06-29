8ca0320b97e49e2bf2bb46b73b87bf9cc266e760
Josh DeCristi <joshdecristi@Joshs-MacBook-Pro.local>
2023-06-29 13:30:09 -0400

    feat: static build for IPFS

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import preprocessor from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
